12568e4902143cd70921734d611f5f3a0680b425
Bastian Lipka <lipka.bastian04@gmail.com>
2025-04-28 19:25:07 +0200

    feat: fetching spotfy api with .ps1 script for testing the outputs, cap and options

---
+++ analysis/spotify_api_fetch.ps1
@@ -0,0 +1,19 @@
+# .env
+$env:CLIENT_ID = Get-Content ".env" | Select-String "CLIENT_ID" | ForEach-Object { $_.Line.Split("=")[1].Trim() }
+$env:CLIENT_SECRET = Get-Content ".env" | Select-String "CLIENT_SECRET" | ForEach-Object { $_.Line.Split("=")[1].Trim() }
+
+# access token
+$tokenResponse = Invoke-RestMethod -Uri "https://accounts.spotify.com/api/token" `
+  -Method Post `
+  -Headers @{ "Authorization" = "Basic " + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$($env:CLIENT_ID):$($env:CLIENT_SECRET)"))} `
+  -Body @{ "grant_type" = "client_credentials" }
+
+$token = $tokenResponse.access_token
+
+# Fetch data from API
+$searchQuery = "test"
+$searchResponse = Invoke-RestMethod -Uri "https://api.spotify.com/v1/search?q=$searchQuery&type=playlist&limit=50" `
+  -Method Get `
+  -Headers @{ "Authorization" = "Bearer $token" }
+
+$searchResponse
