e65d08958f0936a717bd9f0665a9725a79a7b732
Bastian Lipka <lipka.bastian04@gmail.com>
2025-09-28 23:55:28 +0200

    docs: new api usage plane

---
+++ scripts/dynamic_retrieval.go
@@ -0,0 +1,9 @@
+// this go script will use the api in a different way
+// we want to retrieve playlists that contain a specific song
+// as many as we can
+// this data will be handled with also a check for duplicates and stuff
+// the database is supposed to grow with the users that use it
+
+
+// if this is made possible we also should be able to do retrieval with many many more 
+// layers of depth
